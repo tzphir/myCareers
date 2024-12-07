22b8d3b6bbffe6097d6c17916900d69cd1b160e7
Thomas Patenaude <162225330+tzphir@users.noreply.github.com>
2024-12-07 18:19:30 -0500

    Create server.js

---
+++ src/backend/server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express();
+app.use(cors());
+
+
+app.listen(3001, () => {
+
+    console.log("server is running")
+});
+
