d165c1d3d7a455803231368e0a3e9eaf57282781
Thomas Patenaude <162225330+tzphir@users.noreply.github.com>
2024-12-08 00:04:09 -0500

    Update userRoutes.js

--- src/backend/routes/userRoutes.js
+++ src/backend/routes/userRoutes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require("mongoose");
 const UserModel = require("../models/User");
+const bcrypt = require('bcrypt');
+
 
 const router = express.Router();
 
