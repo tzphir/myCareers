317ff3bc86b986a2feb29f62b1b85e0fae5d45b2
Thomas Patenaude <162225330+tzphir@users.noreply.github.com>
2024-12-07 22:21:30 -0500

    Update UserSigninSucess.js

--- src/pages/UserSigninSucess.js
+++ src/pages/UserSigninSucess.js
@@ -5,11 +5,14 @@ import { useNavigate } from 'react-router-dom';
 
 function UserSigninSucess() {
 
+  const navigate = useNavigate();
+
   return (
 
     <div className="login-page"id="Signin">
 
       <p> You have sucessfully created your profile! Please click on the following link to login </p>
+      
       
     
 
