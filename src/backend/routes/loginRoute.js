71eea22893a416c3206134f6b0c3ccceff505385
Thomas Patenaude <162225330+tzphir@users.noreply.github.com>
2024-12-07 22:07:03 -0500

    Update loginRoute.js

--- src/backend/routes/loginRoute.js
+++ src/backend/routes/loginRoute.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
-const UserModel = require('../models/User.js');
+const UserModel = require('../models/User');
 
 const router = express.Router();
 
