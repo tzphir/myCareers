7eb79e718c59f08cf8b8c2960a54aef315ed36d1
Thomas Patenaude <162225330+tzphir@users.noreply.github.com>
2024-12-07 21:40:20 -0500

    Update server.js

--- src/backend/server.js
+++ src/backend/server.js
@@ -4,9 +4,12 @@ const cors = require('cors')
 const app = express();
 app.use(cors());
 
+app.use('/home:id', homeRoute);
+app.use('/job-postings', jobPostingRoutes);
+app.use('/events', eventRoutes);
+
 
 app.listen(5000, () => {
-
     console.log("server is running")
 });
 
