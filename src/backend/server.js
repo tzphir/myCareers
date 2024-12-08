7fcd403e9b4147db4fd21f52a420b57de4b45f9c
Thomas Patenaude <162225330+tzphir@users.noreply.github.com>
2024-12-07 21:41:04 -0500

    Update server.js

--- src/backend/server.js
+++ src/backend/server.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 const app = express();
 app.use(cors());
 
-app.use('/home:id', homeRoute);
+app.use('/home', homeRoute);
 app.use('/job-postings', jobPostingRoutes);
 app.use('/events', eventRoutes);
 
