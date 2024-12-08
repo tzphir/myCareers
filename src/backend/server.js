7c1e4a941fea2be5902deec4176f81f65431eedd
Thomas Patenaude <162225330+tzphir@users.noreply.github.com>
2024-12-08 18:30:25 -0500

    Update server.js

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,16 +1,15 @@
 const express = require('express')
 const cors = require('cors')
+const mongoose = require('mongoose')
+
 
 const app = express();
 app.use(cors());
+app.use(express.json())
 
 const userRoutes = require('./routes/userRoutes');
 
-mongoose.connect("mongodb://localhost:27017/MyCareersDatabase", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}).then(() => console.log('Database connected'))
-  .catch(err => console.error('Database connection error:', err));
+mongoose.connect("mongodb://localhost:27017/MyCareersDatabase");
 
 app.use('/user', userRoutes);
 
@@ -18,4 +17,3 @@ app.use('/user', userRoutes);
 app.listen(5000, () => {
     console.log("server is running")
 });
-
