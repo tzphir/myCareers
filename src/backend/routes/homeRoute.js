9d92f93fddf72144fef8ceee5ae360fbf6054b6f
Thomas Patenaude <162225330+tzphir@users.noreply.github.com>
2024-12-07 21:59:52 -0500

    Update homeRoute.js

--- src/backend/routes/homeRoute.js
+++ src/backend/routes/homeRoute.js
@@ -1,8 +1,20 @@
-const express = require("express");
+const express = require('express');
 const mongoose = require("mongoose");
+const UserModel = require
 
-const router = express.Router();
+// Get User by ID
+router.get('/:userId', (req, res) => {
+    const { userId } = req.params;
 
-router.get("/:id", (req, res) => {
-
+    UserModel.findById(userId)
+        .then(user => {
+            if (!user) {
+                return res.status(404).json({ message: 'User not found' });
+            }
+            res.status(200).json(user);
+        })
+        .catch(err => {
+            console.error(err);
+            res.status(500).json({ message: 'Internal server error' });
+        });
 });
