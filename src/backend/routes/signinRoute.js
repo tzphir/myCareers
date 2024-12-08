6493129e134762109d8d7891bb30c4d4832952a9
Thomas Patenaude <162225330+tzphir@users.noreply.github.com>
2024-12-07 22:00:11 -0500

    Update signinRoute.js

--- src/backend/routes/signinRoute.js
+++ src/backend/routes/signinRoute.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
-const UserModel = require('../models/User');  // Corrected import for UserModel
+const UserModel = require('../models/User');
 const router = express.Router();
 
 mongoose.connect("mongodb://localhost:27017/MyCareersDatabase", {
