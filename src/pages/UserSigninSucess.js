0060714fec38b754d595a430d196ed31a25c9324
Thomas Patenaude <162225330+tzphir@users.noreply.github.com>
2024-12-07 22:23:33 -0500

    Update UserSigninSucess.js

--- src/pages/UserSigninSucess.js
+++ src/pages/UserSigninSucess.js
@@ -3,15 +3,22 @@ import '../styles/Login.css';
 import { useNavigate } from 'react-router-dom';
 
 
+
 function UserSigninSucess() {
 
   const navigate = useNavigate();
+
+  const redirect = () => {
+      navigate('/login');
+  }
 
   return (
 
     <div className="login-page"id="Signin">
 
       <p> You have sucessfully created your profile! Please click on the following link to login </p>
+      <button className="primary-button" onClick={redirect}> Go to login page</button>
+    
       
       
     
