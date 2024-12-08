146d806067a6199ea4b820b5505fa23314d53c33
Thomas Patenaude <162225330+tzphir@users.noreply.github.com>
2024-12-07 22:01:17 -0500

    Update signinRoute.js

--- src/backend/routes/signinRoute.js
+++ src/backend/routes/signinRoute.js
@@ -1,15 +1,8 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const UserModel = require('../models/User');
 const router = express.Router();
 
-mongoose.connect("mongodb://localhost:27017/MyCareersDatabase", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-.then(() => console.log('Database connected'))
-.catch(err => console.error('Database connection error:', err));
 
 router.post('/', (req, res) => {
     const { email, password } = req.body;
