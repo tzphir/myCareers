61de6b45e4f44611d5a800fa46a3bd0cddb1928b
Thomas Patenaude <162225330+tzphir@users.noreply.github.com>
2024-12-08 20:28:16 -0500

    FIX: Changed User.js to respect Shushi's models

--- src/backend/models/User.js
+++ src/backend/models/User.js
@@ -1,75 +1,32 @@
-const mongoose = require('mongoose'); // Importing Schema constructor from mongoose
+const { mongoose } = require('mongoose');
+const { Event } = require('./Event');
+const { JobPosting } = require('./JobPosting');
 
-const UserSchema = new mongoose.Schema({
-
-  // mcgill id
-  id: {type: String, required: false},
-  
-  name: { type: String, required: false },
-  
-  // Email is required and must be unique across all users
-  email: { 
-    type: String, 
-    required: true,
-    unique: true,
-  },
-
-
-  // Password is required during sign-in
-  password: { 
-    type: String, 
-    required: true, 
-  },
-
-  // Faculty field is optional; it can be filled later
-  faculty: { 
-    type: String, 
-    required: false, 
-  },
-
-  // Documents: Array of documents that the user can upload; each document has a required 'id'
-  documents: [
-    {
-      id: { 
-        type: String, 
-        required: true, // Each document needs an 'id'
-      },
-    },
-  ],
-
-  // Events: Array of events the user is associated with, each event has an 'id' and a 'status'
-  events: [
-    {
-      _id: { 
-        type: String, 
-        required: true, // Each event needs an 'id'
-      },
-      status: { 
-        type: String, 
-        enum: ['hidden', 'starred', 'none'], 
-        default: 'none', 
-      },
-    },
-  ],
-
-  // Job Postings: Similar to events, the user can have job postings with an 'id' and 'status'
-  jobPostings: [
-    {
-      _id: { 
-        type: String, 
-        required: true, // Each job posting needs an 'id'
-      },
-      status: { 
-        type: String, 
-        enum: ['hidden', 'starred', 'none'], // Valid status options for the job posting
-        default: 'none', // Default status is 'none'
-      },
-    },
-  ],
+const userSchema = new mongoose.Schema({
+  fname: { type: String, required: true },                          // First name
+  lname: { type: String, required: true },                          // Last name
+  email: { type: String, required: true, unique: true },            // School email
+  id: { type: String, required: true, unique: true },               // Student id
+  password: { type: String, required: true, unique: true },         // Account password
+  faculty: { type: String, required: true },                        // Student faculty
+  documents: [{                                                     // Professional documents (CV, cover letters, transcripts)
+      id: { type: String, required: true },                         // Document name
+      date: { type: Date, required: true },                         // Creation/modification date
+  }],
+  events: [{                                                        // Registered events
+      id: { type: String, required: true },                         // Event name
+      event: { type: Event, required: true, unique: true },         // Event object
+  }],
+  jobPostings: [{                                                   // Job Posting applications
+      id: { type: String, required: true , unique: true },          // Job posting name
+      posting: { type: JobPosting, required: true, unique: true },  // Job posting object
+      status: { type: String, enum: [                               // Job posting status
+          'Pending',        // Waiting for news 
+          'In Progress',    // In interview process
+          'Approved',       // Received offer
+          'Rejected'        // Received rejection
+        ], default: 'In Progress' },
+  }],
 });
 
-// Create the model based on the schema and automatically look up the non-capitalized, plural form of the collection name
-const UserModel = mongoose.model("User", UserSchema);
-
-// Export the model for use in other parts of the application
-module.exports = UserModel;
+module.exports = mongoose.model('User', userSchema, "Users");
