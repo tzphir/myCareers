545e394f2eabfac71dcb8d833e343381081eb819
Thomas Patenaude <162225330+tzphir@users.noreply.github.com>
2024-12-09 11:46:23 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ root.render(
       <Route path="/" element={<Layout />}>
           <Route path="login" element={<Login/>}></Route>
           <Route path="signin" element={<Signin/>}></Route>
-          <Route index element={<Home />} />
+          <Route path="home:/id" element={<Home />} />
           <Route path="usersigninsuccess" element={<UserSigninSuccess />} />
           <Route path="postings+applications" element={<Postings />} />
           <Route path="events" element={<Events />} />
