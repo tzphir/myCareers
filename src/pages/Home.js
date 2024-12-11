d4243946d2f3de933023e1fc9bb2f7222a835483
Thomas Patenaude <162225330+tzphir@users.noreply.github.com>
2024-12-11 10:54:49 -0500

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,7 +7,7 @@ import '../styles/Home.css';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import PieChart from '../components/PieChart';
-import Calendar from 'react-calendar
+import Calendar from 'react-calendar';
 
 function Home() {
 
