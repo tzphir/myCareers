586922b7d55604976116d406659e32de60fb1831
Marie-Christine Sapoundjieva <marie-christine.sapoundjieva@mail.mcgill.ca>
2024-12-12 00:44:00 -0500

    Almost completed the left side of the Postings page and modified slightly the style of the filter window

--- src/pages/myProfile.js
+++ src/pages/myProfile.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import '../styles/MyProfile.css';
 import axios from 'axios';
+//import Postings from './Postings';
+import { useNavigate } from 'react-router-dom';
 
 const MyProfile = () => {
 
@@ -16,8 +18,8 @@ const MyProfile = () => {
         id: '',
         email: '',
         faculty: '',
-        documents: '',
-        jobPostings: ''
+        documents: [],
+        jobPostings: []
     });
 
     const [isChanged, setIsChanged] = useState(false); // Tracks if any input has changed
@@ -26,7 +28,7 @@ const MyProfile = () => {
     useEffect(() => {
         const fetchPersonalInfo = async () => {
             try {
-                const response = await axios.get(`/id:${id}`); 
+                const response = await axios.get(`/id:${localStorage.getItem("id")}`); 
                 setPersonalInfo(response.data);
             } catch (error) {
                 console.error('Error fetching personal info:', error);
@@ -56,6 +58,11 @@ const MyProfile = () => {
             console.error('Error updating personal info:', error);
         }
     };
+
+    const navigate = useNavigate();
+    const goToPostings = () => {
+        navigate('/postings+applications', { replace: true});
+    }
 
     return (
         
@@ -101,21 +108,23 @@ const MyProfile = () => {
                     </ul>
                 </div>
             </div> 
-        
+
+            <div style={{ borderLeft: '3px dashed black', height: '100vh' }} />        
+
             <div className="container" id="personal-info">
                 <div className="types-personal-info">
                     <h2>Personal Info</h2>
                     <form onSubmit={handleSubmit}>
                         <label for="fname">First Name:</label>
-                        <input type='text' name="fname" value={personalInfo.fname} onChange={handleChange}></input><br/>
+                        <input type='text' name="fname" id="fname" value={personalInfo.fname} onChange={handleChange}></input><br/>
                         <label for="lname">Last Name:</label>
-                        <input type='text' name="lname" value={personalInfo.lname} onChange={handleChange}></input>
-                        <label for="studentId">Student ID:</label>
-                        <input type='text' id="studentId" value={personalInfo.id} onChange={handleChange}></input><br/>
+                        <input type='text' name="lname" id="lname" value={personalInfo.lname} onChange={handleChange}></input>
+                        <label for="id">Student ID:</label>
+                        <input type='text' name="id" id="id" value={personalInfo.id} onChange={handleChange}></input><br/>
                         <label for="email">Email:</label>
-                        <input type='text' id="email" value={personalInfo.email} onChange={handleChange}></input><br/>
+                        <input type='text' name="email" id="email" value={personalInfo.email} onChange={handleChange}></input><br/>
                         <label for="faculty">Student ID:</label>
-                        <input type='text' id="faculty" value={personalInfo.faculty} onChange={handleChange}></input><br/>
+                        <input type='text' name="faculty" id="faculty" value={personalInfo.faculty} onChange={handleChange}></input><br/>
                         {isChanged && <input type="submit" value="Submit" />}
                     </form>
                     {/*
@@ -161,14 +170,14 @@ const MyProfile = () => {
                                 <option value='accepted' className="white">Accepted</option>
                                 <option value='rejected' className="white">Rejected</option>
                             </select>
-                            <i class="bi bi-trash"></i>
+                            <i className="bi bi-trash"></i>
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash"  style={{ cursor: 'pointer' }}  viewBox="0 0 16 16">
                                 <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
                                 <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
                             </svg>
                         </li>
                     </ul>
-                    <button type="button" className="search-more-button">Search More</button>
+                    <button type="button" className="search-more-button" onClick={goToPostings}>Search More</button>
                 </div>
             </div>
        </div>
