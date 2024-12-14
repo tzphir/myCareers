1c269fa3de0b16ead383a47186d233b50f4576d9
Thomas Patenaude <162225330+tzphir@users.noreply.github.com>
2024-12-14 15:17:29 -0500

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,17 @@ import React from 'react';
 import '../styles/Template.css';
 import Logo from "../assets/logo.jpeg";
 import { useLocation } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
+
+    
+
+    const id = localStorage.getItem("id");
     const location = useLocation();
+    const navigate = useNavigate();
+
+    const displayNavigation = location.pathname !== '/login' && location.pathname !== '/signin' && location.pathname!== '/usersigninsuccess';
 
     function setHeaderStyle(){
         const links = document.querySelectorAll('.nav_links');
@@ -33,18 +41,20 @@ const Header = () => {
         <header>
             <div id="header" onLoad={setHeaderStyle}>
                 <img src={Logo} alt="MyCareer"/>
-                <button id="logout">Log Out</button>
+                <button id="logout" onClick={() => navigate('/login')}>Log Out</button>
             </div>
             <hr style={{border: '1px solid black'}}/>
-            <nav>
-                <ul>
-                    <li><a href="/" id="home_nav" className="nav_links">Home</a></li>
-                    <li><a href="/postings+applications" className="nav_links" id="postings_nav">Postings/Applications</a></li>
-                    <li><a href="/events" className="nav_links" id="events_nav">Events</a></li>
-                    <li><a href="/myprofile" className="nav_links" id="myprofile_nav">MyProfile</a></li>
-                </ul>
-            </nav>
-            <hr style={{border: '1px solid black'}}/>
+            {displayNavigation && (
+                <nav>
+                    <ul>
+                        <li><a href={`/home`} id="home_nav" className="nav_links">Home</a></li>
+                        <li><a href="/postings+applications" className="nav_links" id="postings_nav">Postings/Applications</a></li>
+                        <li><a href={`/events`} className="nav_links" id="events_nav">Events</a></li>
+                        <li><a href="/myprofile" className="nav_links" id="myprofile_nav">MyProfile</a></li>
+                    </ul>
+                    <hr style={{border: '1px solid black'}}/>
+                </nav>
+            )}
         </header>
     );
 };
