b2ef084cfec473e8057ba2e25f7b6b2e99ca63fb
Thomas Patenaude <162225330+tzphir@users.noreply.github.com>
2024-12-14 15:21:12 -0500

    Update Layout.js

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -4,23 +4,17 @@ import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 const Layout = () => {
-    const pageContainerStyle = {
-        display: 'flex',
-        flexDirection: 'column',
-        height: '100vh', // Ensures the wrapper always fills the viewport height
-    };
-
     return (
-        <div style={pageContainerStyle}>
-            <style>
-                @import url('https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&family=PT+Sans:wght@400;700&display=swap');
-            </style>
+        <>
+        <style>
+            @import url('https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&family=PT+Sans:wght@400;700&display=swap');
+        </style>
             <Header />
-            <main style={{ flex: 1 }}>
+            <main>
                 <Outlet />
             </main>
             <Footer />
-        </div>
+        </>
     );
 };
 
