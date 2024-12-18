8af9910c9c5a36b8bb3619fad43b2776f44f39e9
Thomas Patenaude <162225330+tzphir@users.noreply.github.com>
2024-12-18 16:33:18 -0500

    Shushi, Thomas

--- src/data/models/User.js
+++ src/data/models/User.js
@@ -1,17 +1,19 @@
+//Shushi, Thomas
+
 const { mongoose } = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  fname: { type: String, default: "FirstNamePlaceholder"},                // First name
-  lname: { type: String, default: "LastNamePlaceholder"},                 // Last name
-  email: { type: String, required: true, unique: true },                  // School email
-  id: { type: String, unique: true, default: () => `ID${Date.now()}`},    // Student id
-  password: { type: String, required: true, unique: true },               // Account password
-  faculty: { type: String, default: "FacultyPlaceholder"},                // Student faculty
-  documents: [{                                                           // Professional documents (CV, cover letters, transcripts)
-      id: { type: String, required: true, unique: true },                 // Document name
-      document: { type: String, required: true },                         // Document path
-      date: { type: Date, required: true },                               // Creation/modification date
-      category: { type: String, enum: [                                   // Document type
+  fname: { type: String, default: "FirstNamePlaceholder"},                
+  lname: { type: String, default: "LastNamePlaceholder"},                 
+  email: { type: String, required: true, unique: true },                  
+  id: { type: String, unique: true, default: () => `ID${Date.now()}`},    
+  password: { type: String, required: true, unique: true },               
+  faculty: { type: String, default: "FacultyPlaceholder"},                
+  documents: [{                                                           
+      id: { type: String, required: true, unique: true },                 
+      document: { type: String, required: true },                         
+      date: { type: Date, required: true },                               
+      category: { type: String, enum: [                                   
           "CV",
           "Transcript", 
           "Cover Letter",
@@ -32,12 +34,12 @@ const userSchema = new mongoose.Schema({
       ref: 'JobPosting', 
       required: true 
     },
-    status: { type: String, enum: [                                       // Job posting status
-        "None",           // Didnt apply yet
-        "Pending",        // Waiting for news 
-        "In Progress",    // In interview process
-        "Approved",       // Received offer
-        "Rejected"        // Received rejection
+    status: { type: String, enum: [                                       
+        "None",           
+        "Pending",        
+        "In Progress",    
+        "Approved",       
+        "Rejected"        
       ], default: "None" },
     star: { type: Boolean, required: true, default: false }               // Boolean for if Job is starred or not
   }],
